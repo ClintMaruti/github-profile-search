55d0528c7084e9275c4b0efced41c07d589349c2
ClintPy <cmaruti93@gmail.com>
2019-09-01 13:22:17 +0300

    Finishes UI

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const inputValue = document.querySelector("#first_name")
+const searchButton = document.querySelector("#search")
+const nameContainer = document.querySelector("#name")
+const unContainer = document.querySelector("#user_name")
+const reposContainer = document.querySelector("#repos")
+const urlContainer = document.querySelector("#url")
+
+const client_id =  "Iv1.2693c7d25b24e93c"
+const client_secret = "d18a28b3c22dfb3c4c927857e9f18bcc4161504e"
+
+const fetchUsers = () => {
+    
+}
+
+searchButton.addEventListener("click", () => {
+    console.log("Sasa?")
+})
